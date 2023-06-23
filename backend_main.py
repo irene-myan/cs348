2eee6d33cb9ad1ffe858e26c7a3d2bb5c3689961
JBadges <jackson.brajer@gmail.com>
2023-06-22 23:15:35 -0400

    updated queries

--- backend_main.py
+++ backend_main.py
@@ -32,9 +32,43 @@ def get_db():
 # python3 -m uvicorn backend_main:app --host 0.0.0.0 --port 8000 --reload
 
 # FastAPI
+
+# Return the frequencies of win, draw, loss for a given position
+@app.get("/plist/")
+def get_plist(fen: str, db: Session = Depends(get_db)):
+    query = (
+        "WITH games_including_fen AS( "
+        "    SELECT gid "
+        "    FROM GameMoves gm "
+        "    WHERE gm.fen=: fen "
+        ")"
+        "SELECT result, COUNT(*) AS total_count "
+        "FROM Game g "
+        "JOIN games_including_fen ON g.gid=games_including_fen.gid "
+        "GROUP BY result"
+    )
+
+    result = db.execute(
+        text(query),
+        fen=fen
+    ).fetchall()
+
+    rows = [
+        {
+            "pwhite": float(row[0]),
+            "pdraw": float(row[1]),
+            "pblack": float(row[2]),
+            "total": int(row[3])
+        }
+        for row in result
+    ]
+    return {'games': rows}
+
+
 # Datetime input
 # https://fastapi.tiangolo.com/tutorial/extra-data-types/
 # datetime.date
+# Return the games of the (best elo within a date range)
 @app.get("/topelo/")
 def get_items(date_start: Optional[date] = None, date_end: Optional[date] = None, db: Session = Depends(get_db)):
     if not date_start:
@@ -42,32 +76,23 @@ def get_items(date_start: Optional[date] = None, date_end: Optional[date] = None
     if not date_end:
         date_end = date(9999,9,9)
 
-    querya = (
-        "SELECT CASE "
-        "  WHEN elowhite >= eloblack THEN whiteplayerid "
-        "  ELSE blackplayerid "
-        "END AS top_player_id "
-        "FROM Games "
-        "WHERE date >= :start_date AND date <= :end_date "
-        "ORDER BY top_player_id DESC "
-        "LIMIT 1"
+    query = (
+        "WITH topelo AS ("
+        "  SELECT "
+        "    CASE WHEN white_elo >= black_elo THEN white_elo ELSE black_elo END AS top_elo "
+        "  FROM Games "
+        "  WHERE date >= :start_date AND date <= :end_date"
+        ")"
+        "SELECT g.* "
+        "FROM Games g "
+        "WHERE g.white_elo in topelo.top_elo or g.black_elo in topelo.top_elo"
     )
+
     result = db.execute(
-        text(querya),
+        text(query),
         start_date=date_start,
         end_date=date_end
-    ).fetchone()
-
-    queryb = (
-        "SELECT *"
-        "FROM Games"
-        "WHERE whiteplayerid = :id or blackplayerid = :id"
-    )
-
-    resultb = int(db.execute(
-        text(querya),
-        id=result
-    ).fetchone()[0][0])
+    ).fetchall()
 
     rows = [
         {
@@ -88,13 +113,24 @@ def get_items(date_start: Optional[date] = None, date_end: Optional[date] = None
     ]
     return {'games': rows}
 
-
+# Sort all the games by date
 @app.get("/sbd/")
 def get_items(desc: bool, db: Session = Depends(get_db)):
     order = "DESC" if desc else "ASC"
-    result = db.execute(text(f"SELECT * FROM Games ORDER BY Date {order}"))
+    result = db.execute(text(f"SELECT * FROM Games ORDER BY date {order}"))
     return {'games': [dict(row) for row in result]}
 
+# Count the number of openings and its name
+@app.get("/openings/")
+def get_items(desc: bool, db: Session = Depends(get_db)):
+    query = (
+        "SELECT o.name, COUNT(*) as frequency_played"
+        "From Games g, opened_with ow, Openings o"
+        "WHERE g.gid = ow.gid AND ow.oid = o.oid"
+        "GROUP BY o.name"
+    )
+    result = db.execute(text())
+    return {'games': [dict(row) for row in result]}
 
 @app.get("/test/")
 def get_items(db: Session = Depends(get_db)):
